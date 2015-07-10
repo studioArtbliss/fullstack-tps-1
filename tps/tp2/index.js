ee83335369c8634fd999db2ee63aa38737629157
pelufito <m.pelufito@gmail.com>
2015-07-10 13:50:40 -0300

    Trabajo practico 2

---
+++ tps/tp2/index.js
@@ -0,0 +1,13 @@
+var numeroUsuario = prompt("Ingresa un numero" , 0);
+
+if (numeroUsuario == 0) {
+	alert("El valor ingresado es: " + numeroUsuario);
+}
+if(numeroUsuario < 0){
+
+	alert("El valor ingresado " + numeroUsuario+" es Menor a 0")
+
+}else{
+	alert("El valor ingresado " + numeroUsuario+" es Mayor a 0")
+
+}

417182b414fe07990f2c0ec74a085d141e58abd9
pelufito <m.pelufito@gmail.com>
2015-07-16 19:42:10 -0300

    trabajo practico 3

---
+++ tps/tp3/index.js
@@ -0,0 +1,36 @@
+//Primer parte del TP 3 Invertir orden del String
+
+// Se crean y cargan las variables 
+var cadena = "Cadena de texto";
+var tamanoCadena = cadena.length;
+var cadenaInvertida = "";
+
+//se ejecuta el while usando como parametro el tamaño de la cadena y mediante el metodo charAt se invirte caracter por caracter
+while (tamanoCadena>=0) {
+	cadenaInvertida = cadenaInvertida + cadena.charAt(tamanoCadena);
+	tamanoCadena--;
+}
+//
+console.log(cadena);
+console.log(cadenaInvertida);
+
+//Segunda parte del TP 3 Invertir orden del Array sin utilizar "reverse"
+
+// Definimos variables cargando el array de ejemplo.
+var nuevoArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+//se convierte el array en string
+var arrayAString = nuevoArray.join();
+var tamañoArrayAString = arrayAString.length;
+var stringInvertido = "";
+var nuevoArrayInvertido = [];
+
+//se ejecuta el while usando como parametro el tamaño de la cadena y mediante el metodo charAt se invierte caracter por caracter
+while (tamañoArrayAString>=0) {
+	stringInvertido = stringInvertido + arrayAString.charAt(tamañoArrayAString);
+	tamañoArrayAString--;
+}
+//se vuelve a convertir el string a array
+nuevoArrayInvertido = stringInvertido.split(",");
+console.log(nuevoArray);
+console.log(nuevoArrayInvertido);

ba9da8300cf8b495777ee4b8f86fce69f03027bb
pelufito <m.pelufito@gmail.com>
2015-07-24 18:32:48 -0300

    se agregaron comentarios a index.js

--- tps/tp4/index.js
+++ tps/tp4/index.js
@@ -1,4 +1,4 @@
-
+//declaracion de variables
 var incrementador = 0;
 var str = "";
 var arrayResultado = [];
@@ -7,34 +7,37 @@ var otro = 0;
 var arrayCuenta = [];
 var letras = [];
 
+//funcion de determinar cuales son las vocales de cada palabra
 function contarletras (str) {
-	// body...
-
+	
+	//iteramos por cada palabra 
 	for (var i = 0; i <= str.length; i++) {
-
+		//Se detecta cuales son vocales ya sean mayusculas o minusculas
 		if (str[i] === "a" || str[i] === "A" || 
 			str[i] === "e" || str[i] === "E" ||
 			str[i] === "i" || str[i] === "I" ||
 			str[i] === "o" || str[i] === "O" ||
 			str[i] === "u" || str[i] === "U" ) {
-			
+
 			letras= str[i];
-			console.log (letras);
+			//console.log (letras);
 			incrementador +=  letras.length;
 
 		};
 
 	};
-
+		//se devuelve el valor de cantidad de vocales por palabra
 		return incrementador;
 
 }
 
-
+//se itera para generar el array de resultados
 arrayPalabras.forEach(function(element){
 
 	arrayResultado.push(contarletras(element));
 
 });
 
+// En este momento falla el array porque suma la cantidad de vocales
+//el primer resultado es correcto pero luego suma en lugar de agregarlas
 console.log (arrayResultado);
